1c25298446d096df3bbd29f93c920b9a6a727b37
asura0099 <lphuc7991@gmail.com>
2020-03-21 17:27:02 +0700

    add divisin

--- calculator.py
+++ calculator.py
@@ -13,3 +13,7 @@ def substract(first_term, second_term):
 
 def multiply(first_term, second_term):
     return first_term * second_term
+
+
+def division(first_term, second_term):
+    return first_term \ second_term
